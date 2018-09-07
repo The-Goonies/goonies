3b8a0654b8a598fa45238595c340f415354b9f6c
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-09-07 11:34:53 -0700

    Move react and react-dom from devdependencies to dependencies per eslint recommendations; update README and CONTRIBUTE.md

--- client/source/components/presentational/MapYourRoute.jsx
+++ client/source/components/presentational/MapYourRoute.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class MapYourRoute extends React.Component {
-  constructor (props) {
-    super (props)
+  constructor(props) {
+    super(props);
   }
 
-  render () {
+  render() {
     const divStyle = {
       border: '1px solid black',
       margin: '20px',
-      padding: '50px'
+      padding: '50px',
     };
     return (
       <div>
-        <div style={divStyle}> --Map will go here --
+        <div style={divStyle}>
+          {' '}
+--Map will go here --
         </div>
-        <div style={divStyle}> --Map information will go here --
+        <div style={divStyle}>
+          {' '}
+--Map information will go here --
         </div>
       </div>
-    )
+    );
   }
 }
 
