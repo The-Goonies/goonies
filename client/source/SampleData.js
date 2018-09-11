75ae298aa951eca4c13173eb1be66cccb4f47bce
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-09-10 18:46:22 -0700

    Add very first static Route History page and SampleData file for development

---
+++ client/source/SampleData.js
@@ -0,0 +1,18 @@
+const routes = [
+  {
+    routeName: 'Upper Yosemite Falls',
+    date: new Date(),
+    distanceInMiles: 10.9,
+    timeToCompleteInHours: 20520000,
+    averageSpeedMPH: 10.9 / (20520000 / 3600000),
+  },
+  {
+    routeName: 'Half Dome Yosemite Falls',
+    date: new Date(),
+    distanceInMiles: 20.5,
+    timeToCompleteInHours: 44280000,
+    averageSpeedMPH: 20.5 / 12.3,
+  },
+];
+
+export default routes;
