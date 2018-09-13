5f7d3773808c41d072e1adb7f739c6404fa97a38
Joanne Kim <joanneskim831@gmail.com>
2018-09-12 20:08:11 -0700

    allow user to redirect from login to map upon successful login and user profile is rendered in the background

--- client/source/components/presentational/Login.jsx
+++ client/source/components/presentational/Login.jsx
@@ -61,7 +61,7 @@ class Login extends React.Component {
     const { loggedIn, signedUp } = this.state;
     //  if loggedIn is true, then redirect to /maps page without throwing error
     if (loggedIn) {
-      return <Redirect noThrow to="/user" />;
+      return <Redirect noThrow to="/maps" />;
     }
     if (signedUp) {
       return <Redirect noThrow to="/signUp" />;
