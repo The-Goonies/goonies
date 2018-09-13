4a71b69413224b02289c7cfd341f4c194b11f5bf
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-09-12 22:44:36 -0700

    Comment out a few pieces of stopwatch view to pass linting tests

--- client/source/components/presentational/MapYourRoute.jsx
+++ client/source/components/presentational/MapYourRoute.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import GoogleMapsContainer from './renderMap';
 
 const MapYourRoute = () => (
+  <div>
+    <h3>Your Map</h3>
     <div>
-      <h3>Your Map</h3>
-      <div>
-        <GoogleMapsContainer />
-      </div>
+      <GoogleMapsContainer />
     </div>
+  </div>
 );
 
 
