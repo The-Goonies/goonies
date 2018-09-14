6331fc893370d9c42c8838d5042e11ecba03fc23
Joanne Kim <joanneskim831@gmail.com>
2018-09-14 10:27:15 -0700

    fix syntax errors according to eslint

--- client/source/components/presentational/UserProfile.jsx
+++ client/source/components/presentational/UserProfile.jsx
@@ -4,21 +4,18 @@ class UserProfile extends React.Component {
   constructor({ props, userInfo }) {
     super(props);
 
-    const { username, experience, password } = userInfo;
+    const { username, experience } = userInfo;
 
     this.state = {
       username,
       experience,
-      password,
+      // password,
       oldPassword: '',
       newPassword: '',
       confirmNewPassword: '',
     };
     this.changePassword = this.changePassword.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-  }
-
-  changeUserProfile(e) {
   }
 
   changePassword(e) {
@@ -29,16 +26,15 @@ class UserProfile extends React.Component {
 
   handlePasswordChange() {
     const {
-      username, oldPassword, newPassword, confirmNewPassword,
+      oldPassword, newPassword, confirmNewPassword,
     } = this.state;
-    
     if (newPassword !== confirmNewPassword) {
       alert('Your new password does not match your password confirmation. Please try again');
     }
     if (newPassword === oldPassword) {
       alert('Your new password should not match your old password. Please try again.');
     }
-  } 
+  }
 
   render() {
     const { username, experience } = this.state;
@@ -48,7 +44,7 @@ class UserProfile extends React.Component {
         <form>
           Username:
           { username }
-          <input type="button" value="Change Username" name="newName" onClick={this.changeUserProfile}/>
+          <input type="button" value="Change Username" name="newName" onClick={this.changeUserProfile} />
           <br />
           Experience Level:
           { experience }
