e28b596066f61341631b5b037b4e86860914b0c1
Ryan Heise <heis007ryan@gmail.com>
2018-09-14 22:35:03 -0500

    corrected typo in forcastform

--- client/source/components/__tests__/ForecastForm.spec.jsx
+++ client/source/components/__tests__/ForecastForm.spec.jsx
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import ForecastForm from '../presentational/ForecastForm';
 
 test('App Renders correctly', () => {
-  const component = renderer.create(< ForecastForm />);
+  const component = renderer.create(<ForecastForm />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
