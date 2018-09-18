bcdb9599576fb43c1e655e8154224d49db000a5b
Joanne Kim <joanneskim831@gmail.com>
2018-09-17 18:48:28 -0700

    update read me and attempt changing button styling

--- client/source/components/presentational/SignUp.jsx
+++ client/source/components/presentational/SignUp.jsx
@@ -53,24 +53,28 @@ class SignUp extends React.Component {
     return (
       <div className="login">
         <form>
-          <label htmlFor="username">
-            Username:
-            <input type="text" name="username" onChange={this.handleChange} />
-          </label>
-          <br />
-          <label htmlFor="password">
-            Password:
-            <input type="password" name="password" onChange={this.handleChange} />
-          </label>
-          <br />
-          <p>Experience Level:</p>
-          <select name="experience" value={experience} onChange={this.handleChange}>
-            <option value="Novice">Novice</option>
-            <option value="Intermediate">Intermediate</option>
-            <option value="Advanced">Advanced</option>
-          </select>
-          <br />
-          <input className="signup" type="button" value="Submit" onClick={this.handleSubmit} />
+          <div className="loginfield">
+            <label htmlFor="username">
+              Username:
+              <br />
+              <input type="text" name="username" onChange={this.handleChange} />
+            </label>
+            <br />
+            <label htmlFor="password">
+              Password:
+              <br />
+              <input type="password" name="password" onChange={this.handleChange} />
+            </label>
+            <br />
+            <p>Experience Level:</p>
+            <select name="experience" value={experience} onChange={this.handleChange}>
+              <option value="Novice">Novice</option>
+              <option value="Intermediate">Intermediate</option>
+              <option value="Advanced">Advanced</option>
+            </select>
+            <br />
+            <input className="signup" type="button" value="Submit" onClick={this.handleSubmit} />
+          </div>
         </form>
       </div>
     );
