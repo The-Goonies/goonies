11ce126fb284c5a9ab0cb5eed63d15e30a9016fd
tae-lim <taelim92@gmail.com>
2018-09-21 16:33:31 -0700

    Render map with markers for trailheads

--- client/source/components/presentational/renderMap.jsx
+++ client/source/components/presentational/renderMap.jsx
@@ -1,86 +1,128 @@
-// import React from 'react';
-// // import PropTypes from 'prop-types';
-// // import { GoogleApiWrapper, Map } from 'google-maps-react';
-// // import dotenv from 'dotenv';
-// import axios from 'axios';
+import React from 'react';
+import {
+  withScriptjs, withGoogleMap, GoogleMap, Marker,
+} from 'react-google-maps';
 
+import axios from 'axios';
 
-// // dotenv.config({ path: '../../../../.env' });
-// // import key from '../../../../myapikey';
+// import TrailList from './TrailList.jsx';
 
-// // const apiKey = process.env.REACT_APP_GOOGLE_MAPS_KEY //|| key.key;
+class GoogleMapsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiKey: '',
+      hikingTrails: [
+        {
+          id: 7005207,
+          name: 'Half Dome',
+          type: 'Featured Hike',
+          summary: 'THE premier route in Yosemite. Hike to the top of the most iconic granite dome in the USA.',
+          difficulty: 'black',
+          stars: 4.9,
+          starVotes: 139,
+          location: 'Yosemite Valley, California',
+          url: 'https://www.hikingproject.com/trail/7005207/half-dome',
+          imgSqSmall: 'https://cdn-files.apstatic.com/hike/7035178_sqsmall_1502461759.jpg',
+          imgSmall: 'https://cdn-files.apstatic.com/hike/7035178_small_1502461759.jpg',
+          imgSmallMed: 'https://cdn-files.apstatic.com/hike/7035178_smallMed_1502461759.jpg',
+          imgMedium: 'https://cdn-files.apstatic.com/hike/7035178_medium_1502461759.jpg',
+          length: 14.7,
+          ascent: 4692,
+          descent: -4691,
+          high: 8662,
+          low: 4062,
+          longitude: -119.5583,
+          latitude: 37.7325,
+          conditionStatus: 'All Clear',
+          conditionDetails: '',
+          conditionDate: '2018-08-20 12:20:25',
+        }
+      ]
+    };
+    this.setTrails = this.setTrails.bind(this);
+    this.getTrailsData = this.getTrailsData.bind(this);
+  }
 
-// // const apiKey = process.env.REACT_APP_GOOGLE_MAPS_KEY;
+  componentDidMount() {
+    this.getTrailsData();
+    this.getApiKey();
+  }
 
-// class GoogleMapsContainer extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       // activeMarker: {},
-//       // selectedPlace: {},
-//       apiKey: '',
-//     };
-//     // binding this to event-handler functions
-//     this.onMarkerClick = this.onMarkerClick.bind(this);
-//     this.getApiKey = this.getApiKey.bind(this);
-//   }
+  setTrails(data) {
+    this.setState({
+      hikingTrails: data,
+    });
+  }
 
-//   componentDidMount() {
-//     this.getApiKey();
-//   }
+  getApiKey() {
+    axios.get('/api/key')
+      .then((response) => {
+        console.log('apikey?', response.data);
+        this.setState({
+          apiKey: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error, 'Could not get api key');
+      });
+  }
 
-//   onMarkerClick() {
-//     this.setState({
-//       // selectedPlace: props,
-//       // activeMarker: marker,
-//     });
-//   }
+  getTrailsData() {
+    axios.get('/api/trails')
+      .then((response) => {
+        console.log(response.data.trails);
+        this.setState({
+          hikingTrails: response.data.trails,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error, 'Could not get weather data');
+      });
+  }
 
-//   getApiKey() {
-//     axios.get('/api/key').then((response) => {
-//       console.log('My api key from server: ', response.data);
-//       this.setState({ apiKey: response.data });
-//     }).catch(() => {
-//       console.log('unable to grab api key');
-//     });
-//   }
+  handleMarkerClick(marker) {
+    // use the data from marker when you click on marker
+    console.log('marker', marker);
+    console.log(this);
+  }
 
-//   render() {
-//     const style = {
-//       width: '50vw',
-//       height: '75vh',
-//       marginLeft: 'auto',
-//       marginRight: 'auto',
+  render() {
+    console.log(this.state.hikingTrails)
+    const markers = this.state.hikingTrails.map((trail, i) => (
+      <Marker
+        key={i}
+        position={{
+          lat: trail.latitude,
+          lng: trail.longitude,
+        }}
+        //label={(i + 1).toString()}
+        onClick={() => this.handleMarkerClick(trail)}
+      />
+    ));
+    const YosemiteMap = withScriptjs(withGoogleMap(() => (
+      <GoogleMap
+        defaultZoom={11}
+        defaultCenter={{ lat: 37.749669, lng: -119.555108 }}
+      >
+        {markers}
+      </GoogleMap>
+    )));
+    return (
+      <div>
+        {/* <TrailList hikingTrails={hikingTrails} /> */}
+        <YosemiteMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${this.state.apiKey}`}
+          loadingElement={<div style={{ height: '100%' }} />}
+          containerElement={<div style={{ height: '500px', width: '500px' }} />}
+          mapElement={<div style={{ height: '100%', width: '100%' }} />}
+        />
+      </div>
 
-//     };
-//     const { google } = this.props;
-//     console.log(google);
-//     return (
-//       <div className="map">
-//         <Map
-//           style={style}
-//           // google={google}
-//           zoom={9}
-//           initialCenter={{ lat: 37.749669, lng: -119.555108 }}
+    );
+  }
+}
 
-//         >
-//           {/* <Marker
-//             onClick={this.onMarkerClick}
-//             title="Changing Colors Garage"
-//             position={{ lat: 37.749669, lng: -119.555108 }}
-//             name="Changing Colors Garage"
-//           /> */}
-
-//         </Map>
-//       </div>
-//     );
-//   }
-// }
-
-// // GoogleMapsContainer.propTypes = {
-// //   google: PropTypes.shape({
-// //     maps: PropTypes.object,
-// //   }).isRequired,
-// // };
-
-// export default GoogleMapsContainer;
+export default GoogleMapsContainer;
